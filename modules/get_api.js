c5ca7031b61ab24456fdc747b8a5721a2f854a02
hommyST <hommyst@gmail.com>
2023-08-27 01:31:53 +0300

    bigger test data

--- modules/get_api.js
+++ modules/get_api.js
@@ -1,21 +1,15 @@
 const mimeTypes = require('./content_type')
 
 function getApi(req, res, body) {
-  // console.log(body);
+  let data = require('./test_data.json')
+  console.log(body);
   
   res.setHeader('Content-Type', mimeTypes.json)
 
   switch (req.url.slice(1)) {
     case 'test':
       res.writeHead(200)
-      let data = [
-        { id: 1, test: true, name: 'Вася', age: 21 },
-        { id: 2, test: true, name: 'Петя', age: 25 },
-        { id: 3, test: true, name: 'Коля', age: 23 },
-        { id: 4, test: true, name: 'Иолай', age: 23 },
-        { id: 5, test: true, name: 'Еблаша', age: 27 },
-        { id: 6, test: true, name: 'Коля', age: 23 },
-      ]
+      
 
       Object.keys(body).forEach(key => {
         if (key in data[0]) {
